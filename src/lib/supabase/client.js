1be04b6e2f4a878ca880899fe49183f3301627e8
Akash09082001 <aakashl@letsupgrade.in>
2025-08-25 22:35:44 +0530

    add: supabase client & server connection added

---
+++ src/lib/supabase/client.js
@@ -0,0 +1,13 @@
+import { createBrowserClient } from "@supabase/ssr"
+
+const createClient = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
+
+const supabase = createClient()
+
+export {
+    createClient,
+    supabase
+}
