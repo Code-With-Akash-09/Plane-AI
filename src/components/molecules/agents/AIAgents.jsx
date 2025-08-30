2c6f2486bb3c7fc1da4b5ed3840d7b549c6f245e
Akash09082001 <aakashl@letsupgrade.in>
2025-08-30 17:54:01 +0530

    style: basic create interview & previous interview list ui styled

--- src/components/molecules/agents/AIAgents.jsx
+++ src/components/molecules/agents/AIAgents.jsx
@@ -1,17 +1,14 @@
+import AgentsHeader from "@/components/atoms/agents/AgentsHeader"
 import AiAgentCard from "@/components/atoms/AiAgentCard"
 
 const AIAgents = () => {
     return (
         <>
             <div className="flex flex-col w-full space-y-4 md:space-y-6 py-6 md:py-8 lg:py-10">
-                <div className="w-full space-y-1">
-                    <h2 className="flex font-mono text-xl md:text-2xl lg:text-3xl font-semibold items-center gap-6">
-                        AI Agents <span className="flex flex-grow bg-purple-500 h-1 rounded-full"></span>
-                    </h2>
-                    <p className="text-sm md:text-base">
-                        Your personal team of AI agents, ready to support you anytime, anywhere.
-                    </p>
-                </div>
+                <AgentsHeader
+                    title="AI Agents"
+                    description="Your personal team of AI agents, ready to support you anytime, anywhere."
+                />
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
                     {Agents.map(agent => (
                         <AiAgentCard key={agent.id} agent={agent} />
