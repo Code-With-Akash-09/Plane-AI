9e552d3d1f501bb59167598a373886a30e98c03e
Akash09082001 <aakashl@letsupgrade.in>
2025-09-02 22:14:50 +0530

    add: logout functioality added

--- src/components/organism/agents/AiNavbar.jsx
+++ src/components/organism/agents/AiNavbar.jsx
@@ -3,11 +3,23 @@
 import BackBtn from "@/components/atoms/BackBtn"
 import Logo from "@/components/atoms/Logo"
 import UserAvatar from "@/components/atoms/userAvatar"
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuShortcut, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { useAuth } from "@/providers/AuthProvider"
+import { LogOut } from "lucide-react"
+import { toast } from "sonner"
 
 const AiNavbar = () => {
 
-    const { user } = useAuth()
+    const { user, signOut } = useAuth()
+
+    const handleSignOut = async () => {
+        const { error } = await signOut()
+        if (error) {
+            toast.error(error.message)
+        }
+        toast.success("Logged out successfully!")
+    }
 
     return (
         <>
@@ -18,14 +30,38 @@ const AiNavbar = () => {
                         <Logo link />
                     </div>
                     <div className="flex w-fit">
-                        <UserAvatar
-                            alt={user ? user.user_metadata
-                                ?.name : "John Doe"}
-                            unoptimized={false}
-                            priority
-                            rounded="rounded-full"
-                            className={"size-10"}
-                        />
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="rounded-full">
+                                <div
+                                    className={"flex items-center gap-4 justify-start"}
+                                >
+                                    <UserAvatar
+                                        alt={user ? user.user_metadata
+                                            ?.name : "John Doe"}
+                                        unoptimized={false}
+                                        priority
+                                        rounded="rounded-full"
+                                        className={"size-10"}
+                                    />
+                                </div>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end" className={"min-w-40"}>
+                                <DropdownMenuGroup>
+                                    <DropdownMenuItem asChild>
+                                        <Button
+                                            variant="custom"
+                                            className={"h-[unset] w-full"}
+                                            onClick={handleSignOut}
+                                        >
+                                            Log Out
+                                            <DropdownMenuShortcut>
+                                                <LogOut />
+                                            </DropdownMenuShortcut>
+                                        </Button>
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </div>
                 </div>
             </header>
