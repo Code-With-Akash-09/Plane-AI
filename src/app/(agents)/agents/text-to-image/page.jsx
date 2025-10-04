a70f9bd3e277b7f3ae41ff9a8736093b9230ece8
Akash09082001 <aakashl@letsupgrade.in>
2025-10-04 15:02:42 +0530

    add: pagination added to the generated images

--- src/app/(agents)/agents/text-to-image/page.jsx
+++ src/app/(agents)/agents/text-to-image/page.jsx
@@ -1,14 +1,26 @@
-import ImageGallery from "@/components/molecules/text-to-image/ImageGallery"
+import AgentsHeader from "@/components/atoms/agents/AgentsHeader"
+import ImageCard from "@/components/molecules/text-to-image/ImageCard"
 import { Button } from "@/components/ui/button"
+import { createClient, getUser } from "@/lib/supabase/server"
 import { ArrowRight } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 
-const page = () => {
+const page = async () => {
+
+    const user = await getUser();
+    const supabase = await createClient();
+
+    const { data: Images = [] } = await supabase
+        .from("AIImages")
+        .select("*")
+        .eq("uid", user.id)
+        .order("created_at", { ascending: false })
+        .limit(4)
 
     return (
         <div className="flex-1 px-4 py-4 w-full overflow-y-auto">
-            <div className="flex w-full flex-col max-w-7xl mx-auto h-full gap-4 md:gap-6">
+            <div className="flex w-full flex-col max-w-7xl mx-auto gap-4 md:gap-6">
                 <div className="w-full h-fit space-y-6 md:space-y-8 p-4 md:p-6 lg:p-10 rounded-3xl border border-neutral-800 bg-gradient-to-r from-blue-800/40 via-neutral-900 to-purple-600/30">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 lg:gap-8 w-full h-full max-w-7xl mx-auto">
                         <div className="flex flex-col gap-4 md:gap-6 w-full h-full justify-center">
@@ -42,7 +54,41 @@ const page = () => {
                         </div>
                     </div>
                 </div>
-                <ImageGallery />
+                {
+                    Images?.length > 0 && (
+                        <>
+                            <div className="flex flex-col flex-grow-0 gap-6 w-full">
+                                <AgentsHeader
+                                    title={"Your Created Images"}
+                                    description={"Review and analyze your past images."}
+                                />
+                                <div
+                                    className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full h-fit"
+                                >
+                                    {Images.map((card, i) => (
+                                        <ImageCard
+                                            key={i}
+                                            download={true}
+                                            card={card}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                            <Button
+                                asChild
+                                className={"w-fit mx-auto flex"}
+                            >
+                                <Link
+                                    className="cursor-pointer"
+                                    href={"/agents/text-to-image/gallery"}
+                                >
+                                    See More Imagess
+                                    <ArrowRight />
+                                </Link>
+                            </Button>
+                        </>
+                    )
+                }
             </div>
         </div>
     )
